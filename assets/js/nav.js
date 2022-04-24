e77214421a7d4d4cb10a781216f87909a607ab2d
ElinaSyr <elinasyrr@gmail.com>
2022-04-24 11:03:34 +0300

    header files

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -15,11 +15,13 @@ links_buttons.forEach(element => {
     }
 });
 
-const box = document.querySelector('.navbar-toggler-icon');
+const box = document.querySelector('.navbar-toggler')
+const stop_ = document.querySelector('.svg-header-stop');
+const play = document.querySelector('.svg-header-play');
 let dropmenu = document.getElementById('phone-navbar-nav');
-console.log(box);
 box.addEventListener('click', (e)=>{
-  e.target.classList.toggle('paused');
+  stop_.classList.toggle('hidden');
+  play.classList.toggle('hidden')
   dropmenu.classList.toggle('hidden');
 })
 
@@ -27,3 +29,12 @@ const div1 = document.querySelector('.dropdown-list-identifier');
 div1.addEventListener('click', (e)=> {
   e.target.classList.toggle('hidden');
 })
+
+
+// const box = document.querySelector('.navbar-toggler-icon');
+// let dropmenu = document.getElementById('phone-navbar-nav');
+// console.log(box);
+// box.addEventListener('click', (e)=>{
+//   e.target.classList.toggle('paused');
+//   dropmenu.classList.toggle('hidden');
+// })
