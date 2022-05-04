14b209a9cc1e9dfdd2c0815f75b26c5b21966f7a
ElinaSyr <elinasyrr@gmail.com>
2022-05-04 18:08:06 +0300

    Previous head

--- project/home/assets/js/home.js
+++ project/home/assets/js/home.js
@@ -15,6 +15,36 @@ spans.forEach((span, idx) => {
 	}, 5600 * (idx+1))
 });
 
+// hero 
+
+document.onmousemove = handleMouseMove;
+    function handleMouseMove(event) {
+        var eventDoc, doc, body;
+
+        event = event || window.event; // IE-ism
+
+        // If pageX/Y aren't available and clientX/Y are,
+        // calculate pageX/Y - logic taken from jQuery.
+        // (This is to support old IE)
+        if (event.pageX == null && event.clientX != null) {
+            eventDoc = (event.target && event.target.ownerDocument) || document;
+            doc = eventDoc.documentElement;
+            body = eventDoc.body;
+
+            event.pageX = event.clientX +
+              (doc && doc.scrollLeft || body && body.scrollLeft || 0) -
+              (doc && doc.clientLeft || body && body.clientLeft || 0);
+            event.pageY = event.clientY +
+              (doc && doc.scrollTop  || body && body.scrollTop  || 0) -
+              (doc && doc.clientTop  || body && body.clientTop  || 0 );
+        }
+
+        // Use event.pageX / event.pageY here
+        console.log(event.pageY / window.innerHeight)
+        x = - (event.pageX / window.innerWidth * 100 - 50)
+        y = - (event.pageY / window.innerHeight * 50) + 120
+        modelViewer.cameraOrbit = x +"deg " + y +"deg 0"
+  }
 // Event Statistics 
 const stats = document.getElementById('event_stats')
 function isInViewport(el) {
