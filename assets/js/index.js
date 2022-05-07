e890d594a8dabeb3f3b4672f731fb92d4a79c9a8
G-Papad <geopapadoulis@gmail.com>
2022-05-07 19:54:24 +0300

    sponsors changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,8 +2,6 @@ import "../css/main.scss"
 import "bootstrap"
 
 import "./preloader"
-// import "./nav"
 import "./newsletter"
 import "./lazyload"
 import "./navigation"
-import "@/partners/assets/js/partners.js"
